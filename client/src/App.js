80e4a8b90fe1a6134bcc51911b9d190ab372111d
Will Koury <wgkoury@gmail.com>
2020-08-29 13:57:26 -0500

    A lot of inital stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import axios from 'axios';
-import './App.css';
+import React from "react";
+import io from "socket.io-client";
+import "./App.css";
+
+const socket = io();
 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      connected: ""
+      name: ""
     }
+
+    this.handleLogin = this.handleLogin.bind(this);
   }
   
   componentDidMount() {
-    axios.get("/api/ping")
-    .then(res => {
-      this.setState({
-        connected: res.data
-      });
-    })
+    
+  }
+
+  handleLogin = event => {
+    event.preventDefault();
+    this.setState(({
+      name: event.target.value
+    }));
   }
   
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          
-          <span>{this.state.connected}</span>
-        </header>
+        {this.state.name==="" ? (
+          <div className="initialization">
+            <h4>Enter your name:</h4>
+            <div className="form-fields">
+              <input className="name-input" type="text" placeholder="Name"></input>
+              <button className="name-submit" name="name" onClick={e => this.handleLogin(e)}>Join</button>
+            </div>
+          </div>
+        ) : (
+          <p>Welcome!</p>
+        )}
       </div>
     );
   }
--- server.js
+++ server.js
@@ -1,19 +1,32 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const server = require('http').createServer(app);
+const server = require("http").createServer(app);
 
 const port = process.env.PORT || 8085;
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, "client/build")));
 
-app.get('/api/ping', function (req, res) {
-    return res.send('Backend connected!');
+let players = []; //an array of players connected to the game
+
+//socket.io
+const options = { /* ... */ };
+const io = require("socket.io")(server, options);
+io.on("connection", socket => { 
+    console.log("User connected.")
+
+    socket.on("disconnect", function(){
+        console.log("User disconnected.");
+    });
 });
 
-// Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+app.get("/api/ping", function (req, res) {
+    return res.send("Backend connected!");
+});
+
+// Handles any requests that don"t match the ones above
+app.get("*", (req,res) =>{
+    res.sendFile(path.join(__dirname+"/client/build/index.html"));
 });
 
 server.listen(port, () => {
