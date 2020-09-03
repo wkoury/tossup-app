83a3f1a2ff6046a2e0b0afb0df59e23a503ac457
Will Koury <wgkoury@gmail.com>
2020-09-02 21:44:26 -0500

    Redirects bad routes to home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import Game from "./pages/Game";
 import Admin from "./pages/Admin";
 import "./App.css";
@@ -8,10 +8,13 @@ class App extends React.Component {
   render(){
     return(
       <Router>
-        <Route exact path="/" component={Game} />
-        <Route path="/admin" component={Admin} />
+        <Switch>
+          <Route exact path="/" component={Game} />
+          <Route path="/admin" component={Admin} />
+          <Redirect to="/" />
+        </Switch>
       </Router>
-    )
+    );
   }
 }
 
