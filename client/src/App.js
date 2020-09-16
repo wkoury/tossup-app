23dc963811529a0968c8424a4989cfbd395afaa8
William Koury <wgkoury@gmail.com>
2020-09-15 20:01:55 -0500

    Adds landing page

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import Game from "./pages/Game";
 import Admin from "./pages/Admin";
+import Landing from "./pages/Landing";
 import "./App.css";
 
 class App extends React.Component {
-  render(){
-    return(
+  render() {
+    return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Game} />
+          <Route exact path="/" component={Landing} />
+          <Route path="/game" component={Game} />
           <Route path="/admin" component={Admin} />
           <Redirect to="/" />
         </Switch>
