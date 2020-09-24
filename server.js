6cf275865e52138f9a816fa18c8c90af8296172e
William Koury <wgkoury@gmail.com>
2020-09-24 16:29:39 -0500

    Comments out body parser

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
 const server = require("http").createServer(app);
