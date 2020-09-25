dd1063b6ecd861fc1eb4b2502b9f759ec7bb3614
Will Koury <wgkoury@gmail.com>
2020-09-25 15:50:02 -0500

    Adds title to landing page

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 
 const Landing = () => {
     return (
-        <div className="App">
+        <div className="landing">
+            <h1>Tossup</h1>
+            <p className="subtitle">An easy buzzer system for quiz bowl.</p>
             <Link to="/create">Create a Game</Link>
             <br></br>
             <Link to="/join">Join a Game</Link>
