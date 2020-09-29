4e0e34baff00eea3e7b7d7288c12d9b568fa7d36
Will Koury <wkoury@hey.com>
2020-09-28 21:56:32 -0500

    Changes link colors on hover

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -8,7 +8,7 @@ const Landing = () => {
             <p className="subtitle">An easy buzzer system for quiz bowl.</p>
             <Link to="/join">Join a Game</Link>
             <br></br>
-            <Link style={{ color: "gray" }} to="/create">Create a Game</Link>
+            <Link to="/create">Create a Game</Link>
             <p className="byline">Created by <a href="https://wkoury.com" rel="noopener noreferrer" target="_blank">Will Koury</a>.</p>
         </div>
     );
