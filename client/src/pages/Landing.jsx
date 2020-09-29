a36f7c108094baf95752d27dd10b9f6e925859ba
Will Koury <wkoury@hey.com>
2020-09-28 21:27:34 -0500

    Restyles Landing

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -6,9 +6,9 @@ const Landing = () => {
         <div className="landing">
             <h1>Tossup</h1>
             <p className="subtitle">An easy buzzer system for quiz bowl.</p>
-            <Link to="/create">Create a Game</Link>
-            <br></br>
             <Link to="/join">Join a Game</Link>
+            <br></br>
+            <Link style={{ color: "gray" }} to="/create">Create a Game</Link>
             <p className="byline">Created by <a href="https://wkoury.com" rel="noopener noreferrer" target="_blank">Will Koury</a>.</p>
         </div>
     );
