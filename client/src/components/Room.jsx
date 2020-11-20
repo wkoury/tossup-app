7154c37fa46763da5022e31589eba237aee27b21
Will Koury <wkoury@hey.com>
2020-11-20 10:58:47 -0500

    Changes loader color to white

--- client/src/components/Room.jsx
+++ client/src/components/Room.jsx
@@ -1,16 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Loader from "react-loader-spinner";
 
 const Room = props => {
-    const [loading, setLoading] = useState(".");
-    
-    useEffect(() => {
-        setInterval(() => {
-            loadingGraphic(setLoading);
-        }, 600);
-    },[loading]);
-            
-
     if(props.room){
         return(
             <div className="room">
@@ -23,22 +14,11 @@ const Room = props => {
             <div className="room">
                 <div className="loading">
                     <p>Game Room:</p>
-                    <Loader type="ThreeDots" color="#00BFFF" height={80} width={80} />
+                    <Loader type="ThreeDots" color="#FFFFFF" height={80} width={80} />
                 </div>
             </div>
         );
     }
 }
-
-const loadingGraphic = async(setLoading) => {
-    setLoading("..");
-    await sleep(200);
-    setLoading("...");
-    await sleep(200);
-    setLoading(".");
-    await sleep(200);
-}
-
-const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 
 export default Room;
