bf28f4cc78bee59ee7697a0620be0513c0b968ba
Will Koury <wkoury@hey.com>
2020-11-22 13:39:49 -0500

    Adds loading ellipse to dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import axios from "axios";
+import Loader from "react-loader-spinner";
 import "../App.css";
 
 class Dashboard extends React.Component {
@@ -34,7 +35,7 @@ class Dashboard extends React.Component {
                 <Navbar />
                 <div className="App">
                     <h3>Open rooms:</h3>
-                    <p>{this.state.roomCount}</p>
+                    {this.state.roomCount === "" ? <Loader type="ThreeDots" color="#FFFFFF" height={80} width={80} /> : <p>{this.state.roomCount}</p>}
                 </div>
             </React.Fragment>
         );
