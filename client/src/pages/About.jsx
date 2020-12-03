a412b7c7af61f7459f426862923982a1ae2cc390
Will Koury <wkoury@hey.com>
2020-12-03 18:12:40 -0500

    Adds note about pricing (lack thereof)

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -9,6 +9,7 @@ const About = () => {
                 <div className="about">
                     <p>Tossup is an easy online buzzer system for both virtual and in-person events. </p>
                     <p>Easier to set up and far cheaper than a traditional lockout buzzer, it was created by <a href="https://wkoury.com" rel="noopener noreferrer" target="_blank">Will Koury</a> when he found how unintuitive and clunky other online systems felt.</p>
+                    <p>Tossup and all of its features are free to use and will remain so for the foreseeable future.</p>
                 </div>
             </div>
         </React.Fragment>
