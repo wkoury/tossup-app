d36359f4b16b890221386f5d2642c1d0b4851fda
Will Koury <wkoury@hey.com>
2021-06-26 17:43:19 -0400

    Try no commit and fast forward

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,7 @@ fi
 echo "Updating the production branch..."
 #Update git
 git checkout prod
-git merge --ff master
+git merge --no-commit --ff master
 git push
 git checkout master
 echo "Production branch successfully updated!"
