a9194d8e7bebb19289fe5178e7115217ffe1b58c
Will Koury <wkoury@hey.com>
2021-07-29 19:51:42 -0400

    Allow users to easily copy the room number.

--- client/src/components/Room.jsx
+++ client/src/components/Room.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Loader from "react-loader-spinner";
+import ReactTooltip from "react-tooltip";
+
+const DEFAULT_TEXT = "Click to copy!";
+const TEMPORARY_TEXT = "Room number successfully copied!";
 
 const Room = props => {
+	const [tooltipText, setTooltipText] = useState(DEFAULT_TEXT);
+
+	useEffect(() => {
+		ReactTooltip.rebuild();
+	});
+
+	function copy() {
+		navigator.clipboard.writeText(props.room);
+		setTooltipText(TEMPORARY_TEXT);
+		ReactTooltip.rebuild();
+		setTimeout(() => {
+			setTooltipText(DEFAULT_TEXT);
+			ReactTooltip.rebuild();
+		}, 3000);
+	}
+
 	if (props.room) {
 		return (
 			<div className="room">
+				<ReactTooltip 
+					getContent={[ () => (<span>{tooltipText}</span>), 50 ]}
+				/>
 				<p>Game Room:</p>
-				<h2>{props.room}</h2>
+				<h2
+					onClick={() => { copy() }}
+					data-tip={tooltipText}>
+					{props.room}
+				</h2>
 			</div>
 		);
 	} else {
